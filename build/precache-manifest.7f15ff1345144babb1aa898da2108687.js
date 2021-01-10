17605d43517ee8c1bc1c7f8ea1b38f90adbc600a
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2021-01-10 17:36:48 +0000

    Automatically backed up by Learn

---
+++ build/precache-manifest.7f15ff1345144babb1aa898da2108687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e07f56d4a89ec23bb02829503d65f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "927fbfa44be98a9afa1c",
+    "url": "/static/js/2.97b4407a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.97b4407a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49cffd678834847aa7bc",
+    "url": "/static/js/main.dd2af88d.chunk.js"
+  },
+  {
+    "revision": "b6b7d9f341d802461e6f",
+    "url": "/static/js/runtime-main.b2d6c529.js"
+  }
+]);
